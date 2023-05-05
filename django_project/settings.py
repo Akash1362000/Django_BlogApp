26e4be307477956febd3eb4c9c9b93ad614f379d
Akash Shrivastava <akash.shrivastava136@gmail.com>
2023-05-05 13:10:26 +0530

    AS | Install gunicorn for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Uncomment the below line while deploying the app
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
