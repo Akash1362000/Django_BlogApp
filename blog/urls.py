eb6953c08b4d4207989f8fa4ec9ca45dc60cd0c1
zablon <zablonoigo51@gmail.com>
2024-02-01 06:42:14 +0300

    Chore: Create Post URL CPath

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="blog-home"),
     path("about/", views.about, name="blog-about"),
+    path("create-post/", views.create_post, name="create_post"),
 ]
