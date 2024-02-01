86ea69793105b8fd0913c3585d028308ba1c684c
zablon <zablonoigo51@gmail.com>
2024-02-01 06:42:55 +0300

    Feat: Create Post Logic View

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
-
+from .forms import PostForm
+from django.contrib import messages
 
 def home(request):
     context = {"posts": Post.objects.all()}
@@ -10,3 +11,18 @@ def home(request):
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.author=request.user
+            user.save()
+            messages.success(request, 'The post has been created successfully.')
+            return redirect('home')
+    else:
+        messages.error(request, 'Please correct the following errors:')
+        form=PostForm()
+    return render(request,'blog/create_post.html',{'form':form})
