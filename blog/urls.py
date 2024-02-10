85f6226ed54845fdb72530a4367461ce49a6af5c
zablon <zablonoigo51@gmail.com>
2024-02-10 13:17:58 +0300

    Chore(path): Added update post url configurations

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="blog-home"),
     path("about/", views.about, name="blog-about"),
     path("create-post/", views.create_post, name="create_post"),
+    path("update-post/<int:pk>/", views.update_post, name="update_post"),
 ]
